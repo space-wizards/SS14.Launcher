ede7d78af2df8d0dbea89bcb726a61e38b2cf909
DrSmugleaf <drsmugleaf@gmail.com>
2023-02-03 08:04:07 +0100

    Doc comment

--- SS14.Launcher/Utility/Language.cs
+++ SS14.Launcher/Utility/Language.cs
@@ -4,6 +4,11 @@ namespace SS14.Launcher.Utility;
 
 public static class Language
 {
+    /// <summary>
+    ///     Checks if the user's current culture or UI culture matches the given language.
+    /// </summary>
+    /// <param name="language">Two letter ISO language name, for example "ru".</param>
+    /// <returns>true if either the user's current culture or UI culture match the given language.</returns>
     public static bool UserHasLanguage(string language)
     {
         var thread = Thread.CurrentThread;
