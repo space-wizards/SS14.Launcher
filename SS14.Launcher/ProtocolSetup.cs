14c3eb69ec37814e37b09129fe5f111eb64bd63e
Vasilis The Pikachu <vasilis@pikachu.systems>
2024-01-23 12:05:03 +0100

    Commiting so i can work on my macbook

--- SS14.Launcher/ProtocolSetup.cs
+++ SS14.Launcher/ProtocolSetup.cs
@@ -19,14 +19,21 @@ public static class ProtocolSetup
 
         if (OperatingSystem.IsMacOS())
         {
+            // todo do this (1)
         }
 
         if (OperatingSystem.IsLinux())
         {
+            // todo steam makes its own .desktop and idk if its possible to add mime types to it via steam, so this is a bit of a problem
+            // this will assume you have downloaded the zip launcher
+            // todo how do i get data to see the output of this
             var proc = new Process();
             proc.StartInfo.FileName = "sh";
             proc.StartInfo.Arguments = "xdg-mime default x-scheme-handler/ss14;xdg-mime default x-scheme-handler/ss14";
             proc.Start();
+            // https://stackoverflow.com/questions/206323/how-to-execute-command-line-in-c-get-std-out-results
+            string output = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
         }
 
         return result;
@@ -55,10 +62,20 @@ public static class ProtocolSetup
         }
         if (OperatingSystem.IsMacOS())
         {
+            // todo ditto (1)
+            // https://eclecticlight.co/2019/03/25/lsregister-a-valuable-undocumented-command-for-launchservices/
+            // https://ss64.com/mac/lsregister.html
+
+            var proc = new Process();
+            // Yes you have to manually go to this
+            proc.StartInfo.FileName = "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister";
+            proc.StartInfo.Arguments = "-f /path/to/your/app.app";
+            proc.Start();
         }
 
         if (OperatingSystem.IsLinux())
         {
+            // todo ditto (2)
             var proc = new Process();
             proc.StartInfo.FileName = "xdg-mime";
             proc.StartInfo.Arguments = "xdg-mime default SS14.desktop x-scheme-handler/ss14;xdg-mime default SS14.desktop x-scheme-handler/ss14s";
@@ -86,10 +103,16 @@ public static class ProtocolSetup
         }
         if (OperatingSystem.IsMacOS())
         {
+            // todo ditto (1)
+            var proc = new Process();
+            proc.StartInfo.FileName = "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister";
+            proc.StartInfo.Arguments = "-u /path/to/your/app.app";
+            proc.Start();
         }
 
         if (OperatingSystem.IsLinux())
         {
+            // todo ditto (2)
         }
     }
 }
