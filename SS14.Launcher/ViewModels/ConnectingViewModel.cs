dcec5c4c596b0ac21fed40edf34cf2175d0e18f1
Vasilis The Pikachu <vasilis@pikachu.systems>
2024-10-31 23:38:13 +0100

    Add exception message to the failed update prompt.
    This will make giving support out to players easier without the need to do the logs procedure all the time.

--- SS14.Launcher/ViewModels/ConnectingViewModel.cs
+++ SS14.Launcher/ViewModels/ConnectingViewModel.cs
@@ -161,7 +161,8 @@ public class ConnectingViewModel : ViewModelBase
         _connectorStatus switch
         {
             Connector.ConnectionStatus.None => _loc.GetString("connecting-status-none") + _reasonSuffix,
-            Connector.ConnectionStatus.UpdateError => _loc.GetString("connecting-status-update-error"),
+            Connector.ConnectionStatus.UpdateError => _loc.GetString("connecting-status-update-error",
+                ("err", _updater.ExceptionMessage)),
             Connector.ConnectionStatus.Updating => _loc.GetString("connecting-status-updating", ("status", _loc.GetString(_updaterStatus switch
             {
                 Updater.UpdateStatus.CheckingClientUpdate => "connecting-update-status-checking-client-update",
