2d42647dfb5dfe3ba5d708621397783815a5b35a
ike709 <ike709@github.com>
2024-12-01 09:32:11 -0600

    Update to .NET 9

--- SS14.Launcher/Models/Connector.cs
+++ SS14.Launcher/Models/Connector.cs
@@ -597,7 +597,7 @@ public class Connector : ReactiveObject
             basePath = Path.GetFullPath(Path.Combine(
                 LauncherPaths.DirLauncherInstall,
                 "..", "..", "..", "..",
-                "SS14.Loader", "bin", "Debug", "net8.0"));
+                "SS14.Loader", "bin", "Debug", "net9.0"));
         }
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
