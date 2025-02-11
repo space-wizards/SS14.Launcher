f185c309dba2cbf20cdd3a9a7f22faf98d8f93f3
Visne <39844191+Visne@users.noreply.github.com>
2025-02-12 00:01:18 +0100

    BaseLoginViewModel

--- SS14.Launcher/ViewModels/Login/BaseLoginViewModel.cs
+++ SS14.Launcher/ViewModels/Login/BaseLoginViewModel.cs
@@ -1,22 +1,16 @@
-using ReactiveUI.Fody.Helpers;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
 
 namespace SS14.Launcher.ViewModels.Login;
 
-public abstract class BaseLoginViewModel : ViewModelBase, IErrorOverlayOwner
+public abstract partial class BaseLoginViewModel(MainWindowLoginViewModel parentVM) : ViewModelBase, IErrorOverlayOwner
 {
-    [Reactive] public bool Busy { get; protected set; }
-    [Reactive] public string? BusyText { get; protected set; }
-    [Reactive] public ViewModelBase? OverlayControl { get; set; }
-    public MainWindowLoginViewModel ParentVM { get; }
-
-    protected BaseLoginViewModel(MainWindowLoginViewModel parentVM)
-    {
-        ParentVM = parentVM;
-    }
+    [ObservableProperty] private bool _busy;
+    [ObservableProperty] private string? _busyText;
+    [ObservableProperty] private ViewModelBase? _overlayControl;
+    public MainWindowLoginViewModel ParentVM { get; } = parentVM;
 
     public virtual void Activated()
     {
-
     }
 
     public virtual void OverlayOk()
