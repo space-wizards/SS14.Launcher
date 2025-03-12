d46a099ae5cdbd73f46c71310ae0e85027a57312
Redstones563 <redstones563@outlook.com>
2025-03-12 15:20:48 -0400

    ONE LINE OF CODE FIXED THE LOCAL CONNECTION PROBLEM
    The magic of reading documentation

--- SS14.Launcher/HappyEyeballsHttp.cs
+++ SS14.Launcher/HappyEyeballsHttp.cs
@@ -52,6 +52,7 @@ public static class HappyEyeballsHttp
             //I'm unsure of how to go about this, but having some way to sanity check the proxy before activating it (i.e. a test ping) would be great.
             Uri proxyURI = new Uri(proxyUrl.Trim('"'));
             WebProxy clientProxy = new WebProxy(proxyUrl.Trim('"')); //No clue why .trim('"') is required. C# jank? Doesn't work otherwise.
+            clientProxy.BypassProxyOnLocal = true;
             if (!string.IsNullOrWhiteSpace(proxyURI.UserInfo)){
                 string[] credentials = proxyURI.UserInfo.Split(new[] { ':' });
                 if (credentials.Length > 1){
