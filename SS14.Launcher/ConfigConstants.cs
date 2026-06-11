b0db6ab5b9efd492a7f8ded038aeaa8075fa3dd2
PJB3005 <pieterjan.briers+git@gmail.com>
2026-06-11 17:47:22 +0200

    Disable SS14_LAUNCHER_OVERRIDE_AUTH on release builds
    People already breaking their systems and wasting my time on support requests. Wish I was kidding.

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -62,10 +62,12 @@ public static class ConfigConstants
 
     public const string FallbackUsername = "JoeGenero";
 
+#if DEBUG
     static ConfigConstants()
     {
         var envVarAuthUrl = Environment.GetEnvironmentVariable("SS14_LAUNCHER_OVERRIDE_AUTH");
         if (!string.IsNullOrEmpty(envVarAuthUrl))
             AuthUrl = new UrlFallbackSet([envVarAuthUrl]);
     }
+#endif
 }
