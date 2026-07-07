2313ad05d4c6a77edb3cd86fab04f2c557feb904
Chapien <inquiries@chapien.net>
2026-07-06 21:19:38 -0700

    Copied existing icon.ico to the linux published files via script

--- publish.py
+++ publish.py
@@ -111,7 +111,7 @@ def publish_linux(x64_only: bool):
 
     shutil.copyfile("PublishFiles/SS14.Launcher", "bin/publish/Linux/SS14.Launcher")
     shutil.copyfile("PublishFiles/SS14.desktop", "bin/publish/Linux/SS14.desktop")
-    shutil.copyfile("PublishFiles/SS14.ico", "bin/publish/Linux/SS14.ico")
+    shutil.copyfile("SS14.Launcher/Assets/icon.ico", "bin/publish/Linux/SS14.ico")
 
     shutil.make_archive("SS14.Launcher_Linux", "gztar", "bin/publish/Linux")
 
