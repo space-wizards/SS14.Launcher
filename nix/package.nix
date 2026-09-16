fad31fc895cc09467f8aea23873141a75cba970e
Coca <me@coca.codes>
2026-09-16 17:18:33 +0100

    Add stubs for nix users

---
+++ nix/package.nix
@@ -0,0 +1,2 @@
+# See https://github.com/space-wizards/SS14.Launcher/pull/312 for more clarification
+builtins.throw "SS14.Launcher nix packaging from upstream repository has been removed, please use nixpkgs `space-station-14-launcher` instead."
